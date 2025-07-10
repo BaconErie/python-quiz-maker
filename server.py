a2efef64ad4178cac309d2751a3999e2a374c9b5
BaconErie <baconerie0001@gmail.com>
2025-07-09 20:28:12 -0500

    dynamic assignment_name, directions, test_case

--- server.py
+++ server.py
@@ -6,8 +6,17 @@ app = Flask(__name__)
 def index():
     return 'Hello world'
 
-@app.route('/editor')
-def editor():
-    return render_template('editor.html')
+@app.route('/editor/<name>')
+def editor(name):
+    return render_template('editor.html', assignment_name=name, directions='write fibonacchi', test_case_list=[{
+        'input': '1\n2\n3',
+        'output': 'swg\n\n\n\nsawgsawg'
+    },{
+        'input': '1\n2\n3',
+        'output': 'swg\n\n\n\nsawgsawg'
+    },{
+        'input': '1\n2\n3',
+        'output': 'swg\n\n\n\nsawgsawg'
+    },])
 
-app.run()
+app.run(debug=True)
